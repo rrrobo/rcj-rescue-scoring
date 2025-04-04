18c69ee3e8eff5d7ba8f9d8005f12b70f1dfba3d
Ryo Unemoto <ryorobo@gmail.com>
2025-04-04 20:41:16 +0900

    Remove 2023 and add 2025 route

--- routes/service.js
+++ routes/service.js
@@ -18,6 +18,10 @@ router.get('/editor/simulation/2024', async function (req, res, next) {
   res.render('sim_editor/sim_editor_2024', { user: req.user, pubService: true });
 });
 
+router.get('/editor/simulation/2025', async function (req, res, next) {
+  res.render('sim_editor/sim_editor_2025', { user: req.user, pubService: true });
+});
+
 router.get('/editor/line/:rule', function (req, res, next) {
   const { rule } = req.params;
   res.render(`admin/mapEditor/line_${rule}`, { user: req.user, pubService: true, leagueId: "Line" });
